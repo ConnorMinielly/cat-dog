6478250ef6b162729b8812c5cff6dcb4bb596225
Connor Minielly <connor_minielly@outlook.com>
2024-02-19 23:07:39 -0500

    implemented the rest of the cat-dog API it's testing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,37 @@
 import express from "express";
+import recursiveReplace from "./replace";
 
 const app = express();
 
-// Cat-Dog API root - takes a json and returns a copy with all instances of the value "dog" replaced with "cat"
-app.post("/", (req, res) => {
-  res.status(200).send("All good!");
+app.use(express.json());
+
+// Cat-Dog API - ALL - Give a more useful unsupported method message response.
+app.all("/", (req, res, next) => {
+  if (!["POST", "GET", "HEAD"].includes(req.method)) {
+    res
+      .status(501)
+      .send(
+        `🔴 Unsupported ${req.method} HTTP Method (POST to this endpoint to swap 'dog' values with 'cat')`
+      );
+  }
+  next();
+});
+
+// Cat-Dog API - GET - Gives a basic health check response.
+app.get("/", (req, res, next) => {
+  res
+    .status(200)
+    .send(
+      "🟢 Cat-Dog Server Live! (POST to this endpoint to swap 'dog' values with 'cat')"
+    );
+  next();
+});
+
+// Cat-Dog API - POST - Takes a json and returns a copy with all instances of the value "dog" replaced with "cat".
+app.post("/", (req, res, next) => {
+  const updatedMessage = recursiveReplace(req.body, "dog", "cat");
+  res.status(200).send(updatedMessage);
+  next();
 });
 
 export default app;
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -3,29 +3,110 @@ import { describe, it } from "node:test";
 import request from "supertest";
 import app from "../src/app";
 
+const done = (err, res) => {
+  if (err) throw err;
+};
+
 describe("cat-dog API", () => {
-  it("should respond with a 200 code when receiving a POST with json content", () => {
+  it("should respond with a 200 code when receiving a POST or GET", () => {
+    request(app).post("/").expect(200, done);
+    request(app).get("/").expect(200, done);
+  });
+
+  it("should give a 501 unsupported status code for other methods.", () => {
+    request(app).delete("/").expect(501, done);
+  });
+
+  it("should replace values of 'dog' with 'cat' at the root level of a json structure", () => {
+    const source = {
+      animal1: "cow",
+      animal2: "dog",
+      animal3: "fish",
+      isAnimal: true,
+    };
+
+    const expected = {
+      animal1: "cow",
+      animal2: "cat",
+      animal3: "fish",
+      isAnimal: true,
+    };
+
     request(app)
       .post("/")
-      .expect(200)
-      .end((err, res) => {
+      .send(source)
+      .expect(200, (err, res) => {
         if (err) throw err;
+        assert.deepEqual(res.body, expected);
       });
   });
 
-  it.todo(
-    "should give a standard rejection message for other verbs or routes."
-  );
+  it("should replace values of 'dog' with 'cat' nested deeply within a json structure", () => {
+    const source = {
+      yards: {
+        front: "chicken",
+        back: "dog",
+      },
+      barn: ["horse", "dog", "goat", "dog"],
+      fields: {
+        field1: ["cow", "dog"],
+        field2: ["sheep", "dog", "sheep"],
+      },
+      kennel: "dog",
+      nest: {
+        nest: {
+          nest: {
+            nest: ["dog", "bird"],
+          },
+        },
+      },
+      numberOfAnimals: 14,
+    };
 
-  it.todo(
-    "should replace values of 'dog' with 'cat' at the root level of a json structure",
-    () => {}
-  );
+    const expected = {
+      yards: {
+        front: "chicken",
+        back: "cat",
+      },
+      barn: ["horse", "cat", "goat", "cat"],
+      fields: {
+        field1: ["cow", "cat"],
+        field2: ["sheep", "cat", "sheep"],
+      },
+      kennel: "cat",
+      nest: {
+        nest: {
+          nest: {
+            nest: ["cat", "bird"],
+          },
+        },
+      },
+      numberOfAnimals: 14,
+    };
 
-  it.todo(
-    "should replace values of 'dog' with 'cat' nested deeply within a json structure",
-    () => {}
-  );
+    request(app)
+      .post("/")
+      .send(source)
+      .expect(200, (err, res) => {
+        if (err) throw err;
+        assert.deepEqual(res.body, expected);
+      });
+  });
 
-  it.todo("should return json unchanged if no values match 'dog'", () => {});
+  it("should return json unchanged if no values match 'dog'", () => {
+    const source = {
+      yards: {
+        front: "chicken",
+        back: "goats",
+      },
+    };
+
+    request(app)
+      .post("/")
+      .send(source)
+      .expect(200, (err, res) => {
+        if (err) throw err;
+        assert.deepEqual(res.body, source);
+      });
+  });
 });
